5f0b5f866a1b2a843422ad398247d564b559ff1e
Vinojan Sivajothy <vino_sivajothy@hotmail.com>
2023-05-16 16:38:45 -0600

    added context files

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,29 +1,38 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
+import { Form, Button, Card, } from 'react-bootstrap';
 
-export default function Register  () {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    const [name, setName] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        
-    }
+export default function Register() {
+    const emailRef = useRef()
+    const passwordConfirmRef = useRef()
+    const passwordRef = useRef() 
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className="text-center mb-4" >Sign Up</h2>
+                    <Form>
+                        <Form.Group id="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" ref={emailRef} required/>
+                        </Form.Group>
+                        <Form.Group id="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" ref={passwordRef} required/>
+                        </Form.Group>
+                        <Form.Group id="password-confirm">
+                            <Form.Label>Password Confirmation</Form.Label>
+                            <Form.Control type="password" ref={passwordConfirmRef} required/>
+                        </Form.Group>
+                        <Button className="w-100" type="submit">Sign Up</Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+            <div className="w-100 text-center mt-2">
+                Already have an account? Log In 
+            </div>
 
-return (
-    <div className="auth-form-container">
-    <form className="register-form" onSubmit={handleSubmit}>
-    <label htmlFor="name">Full name</label>
-    <input value={name} name="name" id="name" placeholder="full Name"  />
-    <label htmlFor="email">email</label>
-    <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-    <label htmlFor="password">password</label>
-    <input  value={pass} onChange={(e) => setPass(e.target.value)} type="pass" placeholder="*******" id="password" name="password" />
-    <button type="submit">Register</button>
-    <Link to="/login" className="site-title"> <button className="link-btn"> Already have an account, Login here</button> </Link>
-    </form>
-    
-</div>
-)
+        </>
+    )
 }
